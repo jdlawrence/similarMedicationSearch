e0f56c6ab4008ea1c31037ccc967d5495c6a791b
jdlawrence <jamil.lawrence@gmail.com>
2017-08-04 18:48:32 -0700

    Creates post route for getting form data from client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const rp = require('request-promise');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send(JSON.stringify('cool'));
+});
+
+router.post('/', function (req, res, next) {
+  rp(`https://rxnav.nlm.nih.gov/REST/drugs.json?name=${req.body.medicine}`)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.send(err);
+    });
 });
 
 module.exports = router;
