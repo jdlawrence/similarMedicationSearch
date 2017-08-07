fface1207d10a6a672673de1fa28a07f0ac9e721
jdlawrence <jamil.lawrence@gmail.com>
2017-08-06 17:15:19 -0700

    error handling if drug name is not correct

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,8 @@ class App extends Component {
     super();
     this.state = {
       alternatives: [],
-      searchResults: []
+      searchResults: [],
+      errorInSearch: false
     };
     this.getMedMatches = this.getMedMatches.bind(this);
     this.getAlternatives = this.getAlternatives.bind(this);
@@ -29,27 +30,37 @@ class App extends Component {
     axios.post('/api/search', { medicine })
       .then(data => {
         let results = data.data.drugGroup.conceptGroup[1].conceptProperties;
-        this.setState({ searchResults: results });
+        this.setState({
+          searchResults: results,
+          errorInSearch: false
+        });
       })
       .catch(err => {
-        console.log('errhwere', err);
+        console.log('error in search', err);
+        this.setState({
+          errorInSearch: true,
+          alternatives: []
+        });
       });
   }
   render() {
     return (
       <div className="app">
         <Header />
-        <div className="left-column">
-          <InputForm getMedMatches={this.getMedMatches} />
-          <SearchResults
-            getAlternatives={this.getAlternatives}
-            results={this.state.searchResults}
-          />
-        </div>
-        <div className="right-colum">
-          <Alternatives
-            alternatives={this.state.alternatives}
-          />
+        <div className="column-wrapper">
+          <div className="left-column">
+            <InputForm getMedMatches={this.getMedMatches} />
+            <SearchResults
+              errorInSearch={this.state.errorInSearch}
+              getAlternatives={this.getAlternatives}
+              results={this.state.searchResults}
+            />
+          </div>
+          <div className="right-colum">
+            <Alternatives
+              alternatives={this.state.alternatives}
+            />
+          </div>
         </div>
       </div>
     );
--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import SingleSearchResult from './SingleSearchResult';
 
-const SearchResults = ({ results, getAlternatives }) => {
-  return (
-    <div className="search-results">
-      <div> Search Results:</div>
-      {results.map(result => {
-        return (
-          <SingleSearchResult
-            getAlternatives={getAlternatives}
-            key={result.rxcui}
-            result={result}
-          />
-        );
-      }
-      )}
-    </div>
-  );
+const SearchResults = ({ errorInSearch, results, getAlternatives }) => {
+  if (errorInSearch) {
+    return (
+      <div className="search-results error-in-search">Please enter a valid drug</div>
+    );
+  } else {
+    return (
+      <div className="search-results">
+        <div> Search Results:</div>
+        {results.map(result => {
+          return (
+            <SingleSearchResult
+              getAlternatives={getAlternatives}
+              key={result.rxcui}
+              result={result}
+            />
+          );
+        }
+        )}
+      </div>
+    );
+  }
 };
 export default SearchResults;
